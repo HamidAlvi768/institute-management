bbf92f412bd9b112e9d289e7ba37b5231ff0d0f8
HamidAlvi768 <hamidalvi768@gmail.com>
2025-05-03 17:00:40 +0500

    refactor(dashboard): adjust chart heights and reorganize components
    - Increase chart heights for better visibility across multiple dashboard components
    - Move Sectors chart to a dedicated `SectorsAnalytics` component and update labels in `dountchart.js`
    - Reorganize dashboard layout to include the new `SectorsAnalytics` component

--- src/Pages/Dashboard/index.js
+++ src/Pages/Dashboard/index.js
@@ -13,6 +13,7 @@ import CombinedProgramsChart from "./CombinedProgramsChart";
 import CertificationTrainersChart from "./CertificationTrainersChart";
 import ProgramsTradesChart from "./ProgramsTradesChart";
 import EnrollmentPassoutChart from "./EnrollmentPassoutChart";
+import SectorsAnalytics from "./SectorsAnalytics";
 
 import { Row, Container, Col, Card, CardBody, CardTitle } from "reactstrap";
 
@@ -30,11 +31,12 @@ const Dashboard = () => {
             <OverView />
             {/* Social Source Chart */}
             <SocialSource />
+            <InstituteMetricsChart />
           </Row>
 
           {/* New Dashboard Charts: 3 in one row */}
           <Row>
-            <InstituteMetricsChart />
+            <SectorsAnalytics />
             <EnrollmentAnalyticsChart />
             <ProgramsTradesChart />
           </Row>
@@ -44,6 +46,7 @@ const Dashboard = () => {
             <EmploymentOutcomesChart />
           </Row>
           <Row>
+            <SectorsAnalytics />
             <EnrollmentPassoutChart />
           </Row>
         </Container>
